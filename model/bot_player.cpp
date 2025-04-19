800ea685d5019a0646692b4a3dc970aa3c18cb51
Yash Lad <lad.yas@northeastern.edu>
2025-04-19 19:40:16 -0400

    montecarlo algo implemented

--- model/bot_player.cpp
+++ model/bot_player.cpp
@@ -3,12 +3,13 @@
 #include <cstdlib>
 #include <future>
 #include <random>
+#include <algorithm>
+#include <chrono>
 
 BotPlayer::BotPlayer(const std::string &name, int chips, BotDifficulty diff)
     : Player(name, chips), difficulty(diff) {}
 
-BotDifficulty BotPlayer::getDifficulty() const
-{
+BotDifficulty BotPlayer::getDifficulty() const {
     return difficulty;
 }
 
@@ -25,7 +26,6 @@ bool BotPlayer::shouldCallBet(const std::vector<Card>& fullHand) {
         case BotDifficulty::HardPlus:
             return shouldCallHardPlus(fullHand);
     }
-
     return false;
 }
 
@@ -43,8 +43,47 @@ bool BotPlayer::shouldCallHard(const HandValue& eval) const {
     return (rand() % 5 == 0);  // 20% bluff chance
 }
 
-// We'll build this next
 bool BotPlayer::shouldCallHardPlus(const std::vector<Card>& fullHand) {
-    // TODO: Monte Carlo Simulation
-    return false;
+    const int simulations = 200;
+    const int threads = 4;
+    const int simsPerThread = simulations / threads;
+    std::vector<std::future<int>> futures;
+
+    auto simulate = [&]() -> int {
+        int wins = 0;
+        for (int i = 0; i < simsPerThread; ++i) {
+            std::vector<Card> deck;
+            for (int r = 2; r <= 14; ++r) {
+                for (int s = 0; s < 4; ++s) {
+                    Card c(static_cast<Rank>(r), static_cast<Suit>(s));
+                    if (std::find(fullHand.begin(), fullHand.end(), c) == fullHand.end())
+                        deck.push_back(c);
+                }
+            }
+            std::shuffle(deck.begin(), deck.end(), std::mt19937(std::random_device()()));
+
+            std::vector<Card> opponentHand = { deck[0], deck[1] };
+            std::vector<Card> opponentFullHand = opponentHand;
+            opponentFullHand.insert(opponentFullHand.end(), fullHand.begin() + 2, fullHand.end());
+
+            HandValue opponentEval = AdvancedHandEvaluator::evaluate(opponentFullHand);
+            HandValue botEval = AdvancedHandEvaluator::evaluate(fullHand);
+
+            if (botEval > opponentEval)
+                ++wins;
+        }
+        return wins;
+    };
+
+    for (int i = 0; i < threads; ++i) {
+        futures.push_back(std::async(std::launch::async, simulate));
+    }
+
+    int totalWins = 0;
+    for (auto& f : futures) {
+        totalWins += f.get();
+    }
+
+    double winRate = static_cast<double>(totalWins) / simulations;
+    return winRate >= 0.5;
 }
